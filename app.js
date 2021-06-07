1088d89b2ba1de3c9110f31de6e0fc40b1e96217
goodelias <55098894+goodelias@users.noreply.github.com>
2021-06-07 14:31:31 +0300

    Update package.json

--- app.js
+++ app.js
@@ -225,6 +225,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port, function(){
-    console.log("Server has started on port 3000");
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
 });
